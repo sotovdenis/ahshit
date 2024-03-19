b64241e7fa4c492fd36552021701f0132a138c22
phounexxx <denissotovggg223@gmail.com>
2024-03-19 22:26:43 +0300

    initial plus component http

---
+++ ahshitherewegoagain/ahshitherewegoagain/Controllers/ComponentController.cs
@@ -0,0 +1,27 @@
+﻿using ahshitherewegoagain.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ahshitherewegoagain.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ComponentController : ControllerBase
+    {
+        [HttpGet] 
+        public async Task<ActionResult<List<Component>>> GetAllComponents()
+        {
+            var components = new List<Component>{
+                new Component
+                {
+                    Id = 1,
+                    Name = "Vlad Pidor",
+                    StorageId = 1,
+                    Count = 1,
+                    MinCount = 0,
+                }
+            };
+            return Ok(components);
+        }
+    }
+}
---
+++ ahshitherewegoagain/ahshitherewegoagain/Model/Component.cs
@@ -0,0 +1,11 @@
+﻿namespace ahshitherewegoagain.Model
+{
+    public class Component
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; } = string.Empty;
+        public int StorageId { get; set; }
+        public int Count { get; set; }
+        public int MinCount { get; set; } 
+    }
+}
